9957d8a04f8e965f3a788fcd5006c1b85716dbb4
Utsav Tamrakar <utsavtamrakar54321@gmail.com>
2025-06-06 21:32:19 +0545

    add file

---
+++ 2)identation.py
@@ -0,0 +1,4 @@
+#if 5 > 2 :
+#print("Five is greater than 2!") if there is no identation means space in print then it will show an error..identation means space at the beginning of a code line
+if 5 > 2 :
+   print("Five is greater than 2!")
---
+++ 3)var.py
@@ -0,0 +1,14 @@
+#Variables are containers for storing data values.Python has no command for declaring variables.
+#
+x=5 #assign value and it is an integer
+y="Hello" #assign and it is string
+print(type(x)) # this will show the type of a variables
+print(type(y))
+
+#Variable casting 
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
